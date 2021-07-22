601ba6c6732acc191b1741fd1b447ba1d3476309
Tokov <flyingcatsad@gmail.com>
2021-07-22 21:02:48 +0300

    Add JS template strings

--- index.js
+++ index.js
@@ -1,151 +1,55 @@
-// Functions:
+// JS шаблонные строки
 
 // 1
-function getString(text) {
-    return "Вы ввели " + text;
-}
-
-// 2
-function calcNumPlusFive(num) {
-    return typeof num === "number" ? num + 5 : null;
-}
-
-// 3
-function getConcatOfTwo(str1, str2) {
-    return typeof (str1 && str2) === "string" ? str1 + str2 : null;
-}
-
-// 4
-function calcMultiOfTwo(num1, num2) {
-    return typeof .* === "number" && typeof .* === "number" ? num1 * num2 : null;
-}
-
-// 5
-function isDivisible(num1, num2) {
-    return typeof .* === "number" && typeof .* === "number" ||
-        typeof (num1 && num2) === "string"
-        ? num1 % num2 === 0
-        : false;
-}
-
-// 6
-function raiseToThePower(base, exp) {
-    return typeof .* === "number" && typeof .* === "number" ||
-        typeof (num1 && num2) === "string"
-        ? Math.pow(base, exp)
+/**
+ *
+ * @param {string} country
+ * @returns string
+ */
+function getStringPlusVariable(country) {
+    return typeof country === "string"
+        ? console.log("Я живу в " + country)
         : null;
 }
 
-// 7
-function calcArea(figure) {
-    let S = 0;
-    const p = 3.14;
-
-    switch (figure) {
-        case "triangle":
-            S = (1 / 2) * arguments[1] * arguments[2];
-            break;
-        case "cone":
-            S = p * arguments[1] * (arguments[1] + arguments[2]);
-            break;
-        case "trapezoid":
-            S = (1 / 2) * (arguments[1] + arguments[2]) * arguments[3];
-            break;
-        default:
-            console.log(
-                "Выберите: triangle || cone || trapezoid и введите данные для формулы через запятую!"
-            );
-            return null;
-    }
-
-    return isNaN(S)
-        ? "Введите данные для формулы через запятую, после названия фигуры! "
-        : S;
-}
-
-// 8
-function calcWithOperator(num1, num2, operator) {
-    if (typeof .* === "number" && typeof .* === "number") && typeof operator === "string") {
-        switch (operator) {
-            case "+":
-                return num1 + num2;
-                break;
-            case "-":
-                return num1 - num2;
-                break;
-            case "*":
-                return num1 * num2;
-                break;
-            case "/":
-                return num1 / num2;
-                break;
-            default:
-                console.log("Введите два числа, и оператор строкой!");
-        }
-    } else alert("Введите два числа, и оператор строкой!");
-}
-
-// Functions with Loops:
-
-// 1
-function showHelloLoopMessage() {
-    for (let i = 0; i < 5; i++) {
-        console.log("Hello loop");
-    }
-}
-
 // 2
-function showLoopPlusIMessage() {
-    for (let i = 1; i < 6; i++) {
-        console.log("Loop " + i);
-    }
+/**
+ *
+ * @param {string} username
+ * @param {number} id
+ * @returns username_id
+ */
+function getUsernamePlusId(username, id) {
+    return typeof username === "string" &&
+        typeof id === "number" &&
+        isNaN(id) === false
+        ? console.log(username + "_" + id)
+        : null;
 }
 
 // 3
-function returnNumFiveTimes(num) {
-    if (typeof num === "number") {
-        for (let i = 0; i < 5; i++) {
-            console.log(num);
-        }
-        return num;
-    } else return null;
-}
+/**
+ *
+ * @param {string} capital "London" || "NewYork" || "Minsk"
+ * @param {string} country "GreatBritain" || "USA" || "Belorussia"
+ * @returns capital + "is a capital of" + country
+ */
+function getCapitalOfTheCountry(capital, country) {
+    if (typeof capital !== "string" && country !== "string") {
+        return null;
+    }
 
-// 4
-function multiplyTwoFiveTimes(num1, num2) {
-    if (typeof .* === "number" && typeof .* === "number")) {
-        let multiplied = 0;
+    if (capital === "London" && country === "GreatBritain") {
+        return capital + " is a capital of " + country;
+    }
 
-        for (let i = 0; i < 5; i++) {
-            multiplied += num1 * num2;
-        }
+    if (capital === "NewYork" && country === "USA") {
+        return capital + " is a capital of " + country;
+    }
 
-        return multiplied;
-    } else return null;
-}
+    if (capital === "Minsk" && country === "Belorussia") {
+        return capital + " is a capital of " + country;
+    }
 
-// 5 + 5*
-function calcFactorial(num) {
-    if (typeof num === "number") {
-        let buffer = 1;
-
-        for (let i = 1; i <= num; i++) {
-            buffer *= i;
-        }
-
-        return buffer;
-    } else return null;
-}
-
-// 6*
-function calcPow(val, power) {
-    if (typeof .* === "number" && typeof .* === "number") {
-        let buffer = 1;
-
-        for (let i = 1; i <= power; i++) {
-            buffer *= val;
-        }
-
-        return buffer;
-    } else return null;
+    return null;
 }
