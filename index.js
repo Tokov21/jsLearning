80474db1d03775b695bc87eef6eb65649dd7df83
Tokov <flyingcatsad@gmail.com>
2021-07-25 22:46:28 +0300

    Add objects

--- index.js
+++ index.js
@@ -1,58 +1,72 @@
-// JS шаблонные строки
+// Objects
 
-// 1 Составить строку из слов и переменных `Я живу в ${country}`, вывести в консоль
-/**
- *
- * @param {string} country
- * @returns string
- */
-function getStringPlusVariable(country) {
-    return typeof country === "string"
-        ? console.log(`Я живу в ${country}`)
-        : null;
-}
+// 1. Создать обьект "пользователь" - { }
+// Содержит:
+// свойство firstname = Vasya
+// свойство age = 32
+const user = {
+    firstname: "Vasya",
+    age: 32,
+};
 
-// 2 Составить строку из переменных с названиями firstname и id в формате Ivan_39, вывести в консоль
-/**
- *
- * @param {string} username
- * @param {number} id
- * @returns username_id
- */
-function getUsernamePlusId(username, id) {
-    return typeof username === "string" &&
-        typeof id === "number" &&
-        isNaN(id) === false
-        ? console.log(`${username}_${id}`)
-        : null;
-}
+// 2. Создать объект "кот" - { }
+// свойство длина хвоста
+// метод возвращающий текст "Meow"
+const cat = {
+    tailLength: 10,
+    sayMeow: function () {
+        return "Meow";
+    },
+};
 
-// 3 Составить строки из переменных (названия переменных придумайте), значения переменных:
-// London, NewYork, Minsk,
-// Belorussia, USA, GreatBritain
-// Формат: `${} is a capital of ${}`
-/**
- *
- * @param {string} capital "London" || "NewYork" || "Minsk"
- * @param {string} country "GreatBritain" || "USA" || "Belorussia"
- * @returns capital is a capital of country
- */
-function getCapitalOfTheCountry(capital, country) {
-    if (typeof capital !== "string" && country !== "string") {
-        return null;
-    }
+// 3. Создать объект "жираф" - { }
+// Содержит:
+// Возраст, вес, спит ли жираф, высота туловища, длина шеи,
+// метод рост (возвращает сумму свойств высоты туловища и длины шеи),
+// метод который разбудит или усыпит жирафа.
+const giraffe = {
+    age: 7,
+    weight: 80,
+    isSleeping: false,
+    torsoHeight: 150,
+    neckLength: 50,
+    height: function () {
+        return this.torsoHeight + this.neckLength;
+    },
+    sleep: function () {
+        return this.isSleeping
+            ? (this.isSleeping = false)
+            : (this.isSleeping = true);
+    },
+};
 
-    if (capital === "London" && country === "GreatBritain") {
-        return `${capital} is a capital of ${country}`;
-    }
+// 4. Создать шаблон объекта "мотоцикл" - new Constructor()
+// Содержит:
+// Название модели, объем бака в литрах, модель двигателя, количество мест,
+// текущая скорость, метод для увеличения скорости,
+// метод для замедления скорости, метод полной остановки.
+const Motorcycle = function (
+    model = "",
+    volOfTank = null,
+    engineModel = "",
+    seats = null,
+    speed = null
+) {
+    this.model = model;
+    this.volOfTank = volOfTank;
+    this.engineModel = engineModel;
+    this.seats = seats;
+    this.speed = speed;
+    this.increaseSpeed = function () {
+        return (this.speed += 1);
+    };
+    this.decreaseSpeed = function () {
+        return (this.speed -= 1);
+    };
+    this.engineStop = function () {
+        this.speed = 0;
+        return true;
+    };
+};
 
-    if (capital === "NewYork" && country === "USA") {
-        return `${capital} is a capital of ${country}`;
-    }
-
-    if (capital === "Minsk" && country === "Belorussia") {
-        return `${capital} is a capital of ${country}`;
-    }
-
-    return null;
-}
+const jawa = new Motorcycle('JAWA', 5, 'JS-359', 2, 150);
