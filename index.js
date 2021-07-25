fd3d752ddf23fdb5e30202c6952d5bcc8ee6ac14
Tokov <flyingcatsad@gmail.com>
2021-07-25 22:46:28 +0300

    Add literal strings

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // JS шаблонные строки
 
-// 1
+// 1 Составить строку из слов и переменных `Я живу в ${country}`, вывести в консоль
 /**
  *
  * @param {string} country
@@ -8,11 +8,11 @@
  */
 function getStringPlusVariable(country) {
     return typeof country === "string"
-        ? console.log("Я живу в " + country)
+        ? console.log(`Я живу в ${country}`)
         : null;
 }
 
-// 2
+// 2 Составить строку из переменных с названиями firstname и id в формате Ivan_39, вывести в консоль
 /**
  *
  * @param {string} username
@@ -23,16 +23,19 @@ function getUsernamePlusId(username, id) {
     return typeof username === "string" &&
         typeof id === "number" &&
         isNaN(id) === false
-        ? console.log(username + "_" + id)
+        ? console.log(`${username}_${id}`)
         : null;
 }
 
-// 3
+// 3 Составить строки из переменных (названия переменных придумайте), значения переменных:
+// London, NewYork, Minsk,
+// Belorussia, USA, GreatBritain
+// Формат: `${} is a capital of ${}`
 /**
  *
  * @param {string} capital "London" || "NewYork" || "Minsk"
  * @param {string} country "GreatBritain" || "USA" || "Belorussia"
- * @returns capital + "is a capital of" + country
+ * @returns capital is a capital of country
  */
 function getCapitalOfTheCountry(capital, country) {
     if (typeof capital !== "string" && country !== "string") {
@@ -40,15 +43,15 @@ function getCapitalOfTheCountry(capital, country) {
     }
 
     if (capital === "London" && country === "GreatBritain") {
-        return capital + " is a capital of " + country;
+        return `${capital} is a capital of ${country}`;
     }
 
     if (capital === "NewYork" && country === "USA") {
-        return capital + " is a capital of " + country;
+        return `${capital} is a capital of ${country}`;
     }
 
     if (capital === "Minsk" && country === "Belorussia") {
-        return capital + " is a capital of " + country;
+        return `${capital} is a capital of ${country}`;
     }
 
     return null;
