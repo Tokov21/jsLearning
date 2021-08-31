4e178a2b4c1e2df4c78ca92133966ba8101ae8f1
Tokov <flyingcatsad@gmail.com>
2021-08-31 22:01:34 +0300

    Spread and rest

--- index.js
+++ index.js
@@ -1,184 +1,74 @@
 /*
- * Arrays
- */
-
-/* 
-? 1. Создать пустой массив, ноль ячеек, добавить в него цифры от 1 до 10 (push)
+! spread
 */
-const arr = [];
-for (let i = 1; i <= 10; i++) {
-    arr.push(i);
+function arrayCopy(array) {
+    const newArr = [...array];
+
+    return newArr;
 }
 
-/* 
-? 2. Добавить в начало массива 0
-*/
-arr.unshift(0);
+console.log(arrayCopy([1, 2, 3, 4, 5]));
 
-/* 
-? 3. Удалить 1 элемент с конца массива, и 1 элемент с начала
-*/
-arr.pop();
-arr.shift();
+function concatTwoArrays(arr1, arr2) {
+    return [...arr1, ...arr2];
+}
 
-/* 
-? 4. Удалить 1 элемент где нибудь в середине (splice)
-*/
-arr.splice(4, 1);
+console.log(concatTwoArrays([1, "sas", 3], [1, 2, 3]));
 
-/* 
-? 5. Добавить на его место в середине цифру 5 (splice)
-*/
-arr.splice(4, 0, 5);
+function concatTwoObjects(obj1, obj2) {
+    return { ...obj1, ...obj2 };
+}
 
-/* 
-? 6. Копировать или клонировать весь массив (slice или Object.assign)
-*/
-// * Вариант 1
-// const arrCopy = arr.slice(0);
+console.log(
+    concatTwoObjects(
+        { test: 123, sas: "less", 7: 231 },
+        { 1: 123, 2: "sas", 3: "sas" }
+    )
+);
 
-// * Вариант 2
-const arrCopy = Object.assign([], arr);
+function arrayPlusArrays(array, ...arrays) {
+    const arr = [];
 
-// * Вариант 3
-// const arrCopy = [];
-// Object.assign(arrCopy, arr);
-
-// * Вариант 4
-// ! Та же ссылка (область памяти) в оперативной памяти
-// const arrCopy = Object.assign(arr);
-
-/* 
-? 7. У новой копии массива удалить половину элементов (array.length/2 - 1)
-*/
-function deletePartOfArrCopy() {
-    let arrayPart = Number(
-        prompt("Какую половину массива удалить, 1ю или 2ю?")
-    );
-    let arrCopyLength = arrCopy.length;
-
-    while (arrayPart !== 1 && arrayPart !== 2 && isNaN(arrayPart)) {
-        arrayPart = Number(
-            prompt("Какую половину массива удалить, введите 1 или 2?")
-        );
+    for (let i = 0; i < arrays.length; i++) {
+        arr.push(...arrays[i]);
     }
 
-    if (arrayPart === 1) {
-        for (let i = 0; i < arrCopyLength / 2; i++) {
-            arrCopy.shift();
+    arr.forEach((v, index) => {
+        if (typeof v === "object") {
+            const v1 = { ...v };
+            arr.push(v1);
+            // arr.splice(index, 1, console.log(v1));
         }
-    }
+    });
 
-    if (arrayPart === 2) {
-        for (let i = 0; i < arrCopyLength / 2; i++) {
-            arrCopy.pop();
-        }
-    }
-
-    return arrCopy;
+    return [...array, ...arr];
 }
 
-/* 
-? 8. Создать пустой массив, 8 ячеек + 9 задание
+console.log(
+    arrayPlusArrays(
+        [1, 2, 3],
+        ["obj", 2],
+        [4, { 12: "test" }, 6],
+        ["typo", "test"]
+    )
+);
+
+/*
+! rest
 */
-const newArr = Array(8);
+function arrFromArgs(...array) {
+    const newArr = [...array];
 
-// const newArr = Array(8).fill(0, 0, 4);
-
-/* 
-? 9. Заполнить массив с начала до половины нулями (fill)
-*/
-newArr.fill(0, 0, newArr.length / 2);
-
-/* 
-? 10. Отфильтровать от пустых ячеек (forEach+push / filter)
-*/
-const filteredArr = newArr.filter((value) => value !== undefined);
-
-const forEachArr = [];
-newArr.forEach((value) => forEachArr.push(value));
-
-/* 
-? 11. Создать массив, 10 ячеек, числа от 1 до 10.
-? На основе него создать массив квадратов значений (map).
-*/
-const arrTenNumbers = [];
-
-for (let i = 1; i < 11; i++) {
-    arrTenNumbers.push(i);
+    return newArr;
 }
 
-const arrSquareTenNumbers = arrTenNumbers.map((number) => number * number);
+console.log(arrFromArgs(1, 2, 3, 4, 5));
 
-/* 
-? 12. Перевести массив в строку, с разделителем "&&" (join)
-*/
-const strSquare = arrSquareTenNumbers.join(" && ");
+function deleteOnIndex(deleteIndex, array, ...cells) {
+    const value = array.splice(deleteIndex, 1, ...cells);
+    
 
-/* 
-? 13. Создать массив продуктов, 3-5 элементов. Каждый продукт имеет свойства - наименование, цена, осталось ли на складе, колво на складе.
-*/
-const products = [
-    { id: 1, name: "apple", price: 10, isInStock: true, amount: 3 },
-    { id: 2, name: "juice", price: 30, isInStock: false, amount: 0 },
-    { id: 3, name: "onion", price: 5, isInStock: true, amount: 300 },
-    { id: 4, name: "carrot", price: 3, isInStock: true, amount: 250 },
-    { id: 5, name: "orange", price: 20, isInStock: false, amount: 0 },
-];
-
-/* 
-? 14. Отфильтровать массив продуктов по тому есть ли они на складе. Потом по количеству на складе больше 5 шт. (filter().filter())
-*/
-const filteredProducts = products
-    .filter((value) => value.isInStock === true)
-    .filter((value) => value.amount > 5);
-
-/* 
-? 15. Создать массив Корзина.
-? Спрашивать у пользователя ид (индекс продукта) через prompt который он хочет положить в корзину.
-? _Клонировать_ в корзину обьект продукта по индексу выбранному пользователем.
-*/
-const basket = [];
-
-function addToBasket() {
-    let userProduct = Number(
-        prompt("Введите номер товара (1 - 5), чтобы добавить его в корзину")
-    );
-
-    while (userProduct < 1 || userProduct > 5 || isNaN(userProduct)) {
-        userProduct = Number(
-            prompt(
-                "Вы ввели неправильный номер продукта, введите номер от 1 до 5"
-            )
-        );
-    }
-
-    basket.push(JSON.parse(JSON.stringify(products[userProduct])));
-
-    return basket;
+    return value;
 }
 
-/* 
-? 16. Создать _шаблон_ обьекта Пользователь.
-? Принимать параметры - username, age, isMale, password.
-*/
-const User = function (
-    username = null,
-    age = null,
-    isMale = null,
-    password = null
-) {
-    this.username = username;
-    this.age = age;
-    this.isMale = isMale;
-    this.password = password;
-};
-
-/* 
-? 17. В цикле (for или map) создать 5 пользователей (new User) и поместить их в массив users.
-? Имена, возрасты, пол, и пароль пусть зависят от индекса ("username1", "username2" ...).
-*/
-
-// In progress..
-
-// const users = Array(5).fill([Array(5)].map((value, index) => (value[index] = new User())));
+console.log(deleteOnIndex(3, [1, 2, 3, 4, 5], 1, "sas", 656, 53));
