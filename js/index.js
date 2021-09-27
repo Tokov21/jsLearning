9b272b27c8efca8b21d4b9e58446ece437587219
Tokov <flyingcatsad@gmail.com>
2021-09-27 19:38:47 +0300

    add

--- js/index.js
+++ js/index.js
@@ -1,51 +1,42 @@
-const button1 = document.querySelector("#button1");
+/*
+? Variables
+*/
 
-button1.innerHTML = `Ky
-<div>Privet</div>
-<p>Vot eto prkiol</p>`;
+/* 
+! 1 объявите переменные со значениями “hello js”, 100, 5.5, true
+! Выведите все значения в консоль
+ */
+const hello = "hello js";
+const hundred = 100;
+const fivedotfive = 5.5;
+const right = true;
 
-button1.style.backgroundColor = "aqua";
-button1.style.border = "2px solid blue";
+console.log(`${hello} ${hundred} ${fivedotfive} ${right}`);
 
-const body = document.querySelector("body");
-body.innerHTML = `<div id="newDiv">
-<ul>
-    <li>
-        <li>test 1</li>
-    </li>
-    <li>
-        <li>test 2</li>
-    </li>
-    <li>
-        <li>test 3</li>
-    </li>
-</ul>
-</div>`;
+/*
+! Объявите переменную число 5
+! Выведите в консоль 'На Олимпийском флаге ' + n + ' колец'
+*/
+const num = 5;
 
-const lis = document.getElementsByTagName("li");
-const lis1 = document.querySelectorAll("li");
+console.log(`На олимпийском флаге ${num} колец`);
 
-for (let i = 0; i < lis.length; i++) {
-    lis[i].classList.add("italic");
-}
+/*
+! Объявите три переменные х1, х2, х3. запишите в них числа.
+! Выведите результат формулы x1 + x2 * x3
+*/
+const x1 = 2;
+const x2 = 3;
+const x3 = 4;
 
-for (let i = 0; i < lis1.length; i++) {
-    lis1[i].style.color = "red";
-}
+console.log(x1 + x2 * x3);
 
-console.log(lis);
-console.log(lis1);
+/*
+! объявите необходимые переменные для вычисления площади треугольника по
+! формуле s = 1⁄2 * a * h. выполните вычисления
+*/
+const a = 5;
+const h = 12;
 
-const span = document.createElement("span");
+const s = (1 / 2) * a * h;
 
-span.textContent = "I am new span";
-span.classList.add("span");
-
-body.appendChild(span);
-
-for (let i = 0; i < 3; i++) {
-    const div = document.createElement("div");
-    div.textContent = `divElem${i}`;
-    div.id = `divElem${i}`;
-    body.appendChild(div);
-}
